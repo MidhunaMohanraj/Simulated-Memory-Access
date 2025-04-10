6ab715bc03f0fa364928e7fbb306da69cca7dd48
Destin <destinwong@gmail.com>
2025-04-09 18:03:31 -0700

    Implemented simulator.c and got it to return values

--- simulator.c
+++ simulator.c
@@ -7,14 +7,139 @@
 #include <stdlib.h>
 #include <math.h>
 #include "simulator.h" // implements
+#include <stdbool.h> 
+#include <string.h>
 
 double PI_VAL = 3.14159265358979323846;
 
+// avg_access_time_len is the number of memory modules
 void simulate(double *avg_access_time,
               int avg_access_time_len,
               int procs,
               char dist){
 
+    // define max cycles
+    int max_cycles=1000000;
+    int maxModules = avg_access_time_len;
+    bool random = true;
+    
+    // define arrays to represent processors and memory modules
+    // proc_request is -1 if access granted, set to requested mem module if denied
+    int *proc_request = (int*)malloc(procs * sizeof(int));
+    // count number of times a process is given access
+    int *access_counter = (int*)malloc(procs * sizeof(int));
+    // track which index was denied first last cycle
+    int priority_index = 0;
+
+    // check if dist is normal
+    if(dist == 'n') {
+        random = false;
+    }
+
+    int start_index=0;  //Rotating processor start index
+    for(int numModules = 1; numModules < maxModules + 1; numModules++){
+        // printf("ran moduleloop");
+        // initialize the state of the mem module array
+        // also reset all tracking arrays to 0 or -1
+        int *memory = (int*)malloc(numModules * sizeof(int));
+
+        memset(memory, 0, numModules * sizeof(int));
+        memset(proc_request, -1, procs * sizeof(int));
+        memset(access_counter, 0, procs * sizeof(int));
+
+        priority_index = 0;
+
+        // if the distribution is normal, set the affinity (mean)
+        int *affinity = (int*)malloc(procs * sizeof(int));
+        for(int i = 0; i < procs; i++){
+            affinity[i] = rand_uniform(numModules);
+        }
+      // simulation loop
+      for(int cycle=0; cycle < max_cycles; cycle++){
+        // a variable to keep track if a processor has been denied yet
+        // upon first denying a processor, set this to true and set priority_index to the denied processor
+        bool denied = false;
+        memset(memory, 0, numModules * sizeof(int));
+
+            for(int i=0;i<procs;i++){
+                // get the index, but added to priority index and wrapped around
+                int wrapped_processor_index = (priority_index + i) % procs;
+                if(proc_request[wrapped_processor_index] != -1){ // if processor was denied, keep processor's request the same
+                    // if the requested mem is empty, set it to filled, and set its request to -1 (granted)
+                    if (memory[proc_request[wrapped_processor_index]] == 0){
+                        memory[proc_request[wrapped_processor_index]] = 1;
+                        proc_request[wrapped_processor_index] = -1;
+                        access_counter[wrapped_processor_index] += 1;
+                    }else{ // if request denied again, then if this was the first denied, set priority
+                        if (denied == false){
+                            denied = true;
+                            priority_index = wrapped_processor_index;
+                        }
+                    }
+                }
+                else{ // send a request to a random/normal dist mem module
+                    if (random == true){ // random uniform
+                        // proc_request[wrapped_processor_index] = rand_uniform(numModules);
+                        int requested_memory = rand_uniform(numModules);
+                        if (memory[requested_memory] == 0){
+                            memory[requested_memory] = 1;
+                            proc_request[wrapped_processor_index] = -1;
+                            access_counter[wrapped_processor_index] += 1;
+                        }else{ // if request denied again, then if this was the first denied, set priority
+                            proc_request[wrapped_processor_index] = requested_memory;
+                            if (denied == false){
+                                denied = true;
+                                priority_index = wrapped_processor_index;
+                            }
+                        }
+                    }else{ //  normal distribution
+                        int requested_memory = rand_normal_wrap(affinity[wrapped_processor_index],5,numModules);
+                        if (memory[requested_memory] == 0){
+                            memory[requested_memory] = 1;
+                            proc_request[wrapped_processor_index] = -1;
+                            access_counter[wrapped_processor_index] += 1;
+                        }else{ // if request denied again, then if this was the first denied, set priority
+                            proc_request[wrapped_processor_index] = requested_memory;
+                            if (denied == false){
+                                denied = true;
+                                priority_index = wrapped_processor_index;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+  
+        // calculate the average access time for all processors
+        // insert it into the right spot into avg_access_time array
+        double access_time_total = 0.0;
+        for (int i = 0; i < procs; i++){
+            if(access_counter[i]!=0){
+            access_time_total += ((float) max_cycles) / access_counter[i];
+            }else{
+                printf("were starving");
+            }
+        }
+        double avg_access_time_for_this_mem = ((float) access_time_total) / procs;
+        avg_access_time[numModules - 1] = avg_access_time_for_this_mem;
+
+        free(memory);
+        free(affinity);
+
+
+
+
+    
+    
+    }
+    free(proc_request);
+    free(access_counter);
+    
+            
+
+            
+        
+    
     //////////////////////////////////////////////////////
     // YOUR CODE GOES HERE.
     // start of useless code. Just delete it.
